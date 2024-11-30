2661fe87607ab341afbae9c4f86c8b29a94445d1
Jonel Cubio <jonelcubio021@gmail.com>
2024-11-30 21:07:10 +0800

    Add files via upload

---
+++ register.js
@@ -0,0 +1,13 @@
+function togglePassword () {
+  const passwordInput = document.querySelector('.js-input-password');
+
+  if (passwordInput.type === 'password') {
+    passwordInput.type = 'text';
+  } else {
+    passwordInput.type = 'password';
+  }
+}
+
+document.querySelector('.js-input-password').addEventListener('click', () => {
+  togglePassword ();
+});
